906825294f7b9d59a17abbec0dfe612c08c52794
manasasrinivas-97 <105203982+manasasrinivas-97@users.noreply.github.com>
2022-06-15 15:54:14 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 #include "ColorPair.h"
 
 int main() {
-    TelCoColorCoder::ColorPair  *Obj = new TelCoColorCoder::ColorPair (TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
-    Obj->testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
-    Obj->testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
+    
+ testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
+    testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
 
-    Obj->testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
-    Obj->testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
+   testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
+    testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
 
     return 0;
 }
