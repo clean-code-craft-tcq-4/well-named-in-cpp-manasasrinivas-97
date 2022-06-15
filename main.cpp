dde94b0201243d3d389c7875b5cf1639fd8270c6
manasasrinivas-97 <105203982+manasasrinivas-97@users.noreply.github.com>
2022-06-15 15:45:12 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,22 @@
 #include <iostream>
 #include <assert.h>
 #include "ColorPair.h"
+namespace TelCoColorCoder
+{
+    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+    const char* MajorColorNames[] = {
+        "White", "Red", "Black", "Yellow", "Violet"
+    };
+    int numberOfMajorColors =
+        sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    const char* MinorColorNames[] = {
+        "Blue", "Orange", "Green", "Brown", "Slate"
+    };
+    int numberOfMinorColors =
+        sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+}
 int main() {
     TelCoColorCoder::ColorPair  *Obj = new TelCoColorCoder::ColorPair (TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
     Obj->testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
