25010349ecd74d7c0b28465fa2c52742de74edb1
manasasrinivas-97 <105203982+manasasrinivas-97@users.noreply.github.com>
2022-06-15 15:51:04 +0530

    Update ColorPair.h

--- ColorPair.h
+++ ColorPair.h
@@ -18,7 +18,7 @@ namespace TelCoColorCoder
     };
     int numberOfMinorColors =
         sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-}
+
     class ColorPair {
         private:
             MajorColor majorColor;
@@ -42,4 +42,5 @@ namespace TelCoColorCoder
             ColorPair GetColorFromPairNumber(int pairNumber);
     
 };
+}
 #endif //ColorPair__H___
