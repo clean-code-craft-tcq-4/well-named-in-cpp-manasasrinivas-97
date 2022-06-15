5072ea97bfd235f5a06452d0934e7afd37a11c9e
manasasrinivas-97 <105203982+manasasrinivas-97@users.noreply.github.com>
2022-06-15 18:26:03 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -21,12 +21,23 @@ void testPairToNumber(
     std::cout << "Got pair number " << pairNumber << std::endl;
     assert(pairNumber == expectedPairNumber);
 }
+void printColorCodeManual() 
+{
+	std::cout << "Color Coding Reference Manual: " << std::endl;
+	std::cout << "Pair No." << "    " << "Color_Pair" << std::endl;
+	for (int pairNumber = 1; pairNumber <= 25; pairNumber++)
+	{
+		TelCoColorCoder::ColorPair colorPair = TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+		std::cout << pairNumber << "    " << colorPair.ToString() << std::endl;
+	}
+}
 int main() {
+    printColorCodeManual();
     
- testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
+    testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
     testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
 
-   testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
+    testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
     testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
 
     return 0;
