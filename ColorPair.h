b22454494e16b444d940e32aa61677687a5ae69d
manasasrinivas-97 <105203982+manasasrinivas-97@users.noreply.github.com>
2022-06-15 18:28:15 +0530

    Update ColorPair.h

--- ColorPair.h
+++ ColorPair.h
@@ -4,21 +4,7 @@
 #include <iostream>
 #include <assert.h>
 namespace TelCoColorCoder
-{
-    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
-
-    const char* MajorColorNames[] = {
-        "White", "Red", "Black", "Yellow", "Violet"
-    };
-    int numberOfMajorColors =
-        sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-    const char* MinorColorNames[] = {
-        "Blue", "Orange", "Green", "Brown", "Slate"
-    };
-    int numberOfMinorColors =
-        sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
+{   
     class ColorPair {
         private:
             MajorColor majorColor;
@@ -40,6 +26,7 @@ namespace TelCoColorCoder
                 return colorPairStr;
             }
             ColorPair GetColorFromPairNumber(int pairNumber);
+            int GetPairNumberFromColor(MajorColor major, MinorColor minor);
     
 };
 }
