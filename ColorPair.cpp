19570f78ea149231b3ed13f30742abc814ca5ce3
manasasrinivas-97 <105203982+manasasrinivas-97@users.noreply.github.com>
2022-06-15 18:14:48 +0530

    Update ColorPair.cpp

--- ColorPair.cpp
+++ ColorPair.cpp
@@ -14,20 +14,3 @@ ColorPair ColorPair::GetColorFromPairNumber(int pairNumber) {
         return major * numberOfMinorColors + minor + 1;
     }
 
-void testNumberToPair(int pairNumber,
-    TelCoColorCoder::MajorColor expectedMajor,
-    TelCoColorCoder::MinorColor expectedMinor) {
-   ColorPair colorPair = ColorPair::GetColorFromPairNumber(pairNumber);
-    std::cout << "Got pair " << colorPair.ToString() << std::endl;
-    assert(colorPair.getMajor() == expectedMajor);
-    assert(colorPair.getMinor() == expectedMinor);
-}
-
-void testPairToNumber(
-    TelCoColorCoder::MajorColor major,
-    TelCoColorCoder::MinorColor minor,
-    int expectedPairNumber) {
-    int pairNumber = GetPairNumberFromColor(major, minor);
-    std::cout << "Got pair number " << pairNumber << std::endl;
-    assert(pairNumber == expectedPairNumber);
-}
